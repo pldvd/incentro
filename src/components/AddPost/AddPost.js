c7efd28d1d6605a21d1c973dca59914ff4dbb443
David Pal <paldavid2030@gmail.com>
2020-02-17 11:43:53 +0300

    add post functionality

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function AddPost() {
 
-  const handleSubmit = (formData) => {
+  const [postTitle, setPostTitle] = useState(null);
+  const [postBody, setPostBody] = useState(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      postTitle,
+      postBody
+    }
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .then(response => console.log(response));
+  }
+
+  const handleTitleChange = (e) => {
+    setPostTitle(e.target.value);
+  }
+
+  const handlePostBodyChange = (e) => {
+    setPostBody(e.target.value);
   }
 
   return (
     <div>
       <h1>Submit New Post</h1>
       <form method="/" onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Add title" />
-        <input type="text" name="body" placeholder="Add post body" />
+        <label htmlFor="title">Add a post title</label>
+        <input type="text" name="title" id="title" placeholder="Add title" onChange={e => handleTitleChange(e)} />
+        <label htmlFor="body">Add post body text</label>
+        <input type="text" name="body" id="body" placeholder="Add post body" onChange={e => handlePostBodyChange(e)} />
         <input type="submit" />
       </form>
     </div>
